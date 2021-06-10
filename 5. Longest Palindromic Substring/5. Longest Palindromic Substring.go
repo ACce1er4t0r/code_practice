a04724059e0c7504c4645de33c5089d91201e3b1
VctorControl <776997349@qq.com>
2021-06-11 01:43:29 +0800

    5 second solution

--- 5. Longest Palindromic Substring/5. Longest Palindromic Substring.go
+++ 5. Longest Palindromic Substring/5. Longest Palindromic Substring.go
@@ -1,7 +1,10 @@
 package lengthOfLongestSubstring
 
 // first
-func longestPalindrome(s string) string {
+func longestPalindrome1(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
 	res := ""
 	for i := 0; i < len(s); i++ {
 		res = maxPalindrome(s, i, i, res)
@@ -20,4 +23,27 @@ func maxPalindrome(s string, i int, j int, res string) string {
 		return sub
 	}
 	return res
+}
+
+// second
+func longestPalindrome2(s string) string {
+	if len(s) <= 0 {
+		return ""
+	}
+	left, right, l, r := 0, -1, 0, 0
+	for left < len(s) {
+		for right+1 < len(s) && s[left] == s[right+1] {
+			right++
+		}
+		for left-1 >= 0 && right+1 < len(s) && s[left-1] == s[right+1] {
+			left--
+			right++
+		}
+		if right-left > r-l {
+			l, r = left, right
+		}
+		left = (left+right)/2 + 1
+		right = left
+	}
+	return s[l : r+1]
 }
