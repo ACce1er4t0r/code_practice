efc00788ae652748098f4c43f818e09680e4786e
VctorControl <776997349@qq.com>
2021-07-04 17:09:17 +0800

    17

--- 17. Letter Combinations of a Phone Number/17. Letter Combinations of a Phone Number.go
+++ 17. Letter Combinations of a Phone Number/17. Letter Combinations of a Phone Number.go
@@ -1,5 +1,32 @@
 package letterCombinations
 
-func letterCombinations(digits string) []string {
+var letterMap = []string{
+	" ",    //0
+	"",     //1
+	"abc",  //2
+	"def",  //3
+	"ghi",  //4
+	"jkl",  //5
+	"mno",  //6
+	"pqrs", //7
+	"tuv",  //8
+	"wxyz", //9
+}
 
+func letterCombinations(digits string) []string {
+	if digits == "" {
+		return []string{}
+	}
+	result := []string{""}
+
+	for i := 0; i < len(digits); i++ {
+		tmp := []string{}
+		for j := 0; j < len(result); j++ {
+			for k := 0; k < len(letterMap[digits[i]-'0']); k++ {
+				tmp = append(tmp, result[j]+string(letterMap[digits[i]-'0'][k]))
+			}
+		}
+		result = tmp
+	}
+	return result
 }
